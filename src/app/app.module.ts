178744ee410d897f27011e15b19aaec556eefb1b
chiefnaheem <naheemademola21@gmail.com>
2023-03-18 09:05:23 -0700

    connected db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      keepConnectionAlive: true,
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT || 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
