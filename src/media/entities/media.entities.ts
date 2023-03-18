a8fce8b4bfc7f78d7dd07f4a48230e187da98091
chiefnaheem <naheemademola21@gmail.com>
2023-03-18 09:28:38 -0700

    modelled db

--- src/media/entities/media.entities.ts
+++ src/media/entities/media.entities.ts
@@ -1,11 +1,33 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { MediaStatusEnum, MediaTypeEnum } from "../enum/media.enum";
 
 @Entity()
 export class Media {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: false })
+    @Column({ type: 'enum', enum: MediaTypeEnum })
+    type: MediaTypeEnum;
 
+    @Column({ nullable: false })
+    name: string;
+
+    @Column({ nullable: true, default: null, type: 'text',  })
+    description: string;
+
+    @Column({ nullable: false })
+    url: string;
+
+    @Column({ type: 'enum', enum: MediaStatusEnum })
+    status: MediaStatusEnum;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    @Column({ nullable: true, default: null })
+    deletedAt: Date;
 
 }
--- src/media/enum/media.enum.ts
+++ src/media/enum/media.enum.ts
@@ -2,3 +2,8 @@ export enum MediaTypeEnum {
   IMAGE = 'image',
   AUDIO = 'audio',
 }
+
+export enum MediaStatusEnum {
+    ACTIVE = 'active',
+    INACTIVE = 'inactive',
+}
