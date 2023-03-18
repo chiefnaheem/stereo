e38bd28d32037fd90e4e91b065da51e41b40a36b
chiefnaheem <naheemademola21@gmail.com>
2023-03-18 09:30:03 -0700

    started writing service

--- src/media/entities/media.entities.ts
+++ src/media/entities/media.entities.ts
@@ -1,33 +1,38 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { MediaStatusEnum, MediaTypeEnum } from "../enum/media.enum";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { MediaStatusEnum, MediaTypeEnum } from '../enum/media.enum';
 
 @Entity()
 export class Media {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column({ type: 'enum', enum: MediaTypeEnum })
-    type: MediaTypeEnum;
+  @Column({ type: 'enum', enum: MediaTypeEnum })
+  type: MediaTypeEnum;
 
-    @Column({ nullable: false })
-    name: string;
+  @Column({ nullable: false })
+  name: string;
 
-    @Column({ nullable: true, default: null, type: 'text',  })
-    description: string;
+  @Column({ nullable: true, default: null, type: 'text' })
+  description: string;
 
-    @Column({ nullable: false })
-    url: string;
+  @Column({ nullable: false })
+  url: string;
 
-    @Column({ type: 'enum', enum: MediaStatusEnum })
-    status: MediaStatusEnum;
+  @Column({ type: 'enum', enum: MediaStatusEnum })
+  status: MediaStatusEnum;
 
-    @CreateDateColumn()
-    createdAt: Date;
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @UpdateDateColumn()
-    updatedAt: Date
+  @UpdateDateColumn()
+  updatedAt: Date;
 
-    @Column({ nullable: true, default: null })
-    deletedAt: Date;
-
+  @Column({ nullable: true, default: null })
+  deletedAt: Date;
 }
